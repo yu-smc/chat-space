04d171cc4ae44b993bf44f511061616fc3dbe133
yu <yu.smc.roundabout@gmail.com>
2018-05-23 00:26:06 +0900

    [add] change settings of application - not to automatically create files when creating controllers

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module ChatSpace
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+        # コントローラ生成時に不要なファイルを生成しない
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
