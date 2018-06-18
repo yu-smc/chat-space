a01c2e3c299849ee0476354b5edd6622337f8c31
yu <yu.smc.roundabout@gmail.com>
2018-06-18 18:00:13 +0900

    [add] add js file for asynchronous communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,52 @@
+$(function() {
+  function buildHTML(message){
+    var insertBody = '';
+    var insertImage = '';
+    if (message.body) {
+      insertBody = `<p>${message.body}</p>`;
+    }
+    if (message.image.url) {
+      insertImage = `<img src="${message.image.url}" class="message-image">`;
+    }
+    var html = `<div class="message">
+                  <div class="message-info">
+                    <p class="message-info__user">
+                      ${message.user_name}
+                    </p>
+                    <p class="message-info__date">
+                      ${message.created_at}
+                    </p>
+                  </div>
+                  <div class="message-text">
+                    ${insertBody}
+                    ${insertImage}
+                  </div>
+                </div>`
+    return html;
+  }
+  $("#new_message").on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var $messages = $('.main-wrapperForContents');
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form-items__input-inner').val('');
+      $messages.animate({ scrollTop: $messages.get(0).scrollHeight },'fast');
+    })
+    .fail(function(){
+      alert('エラーが発生しました。入力内容をご確認ください。');
+    })
+  return false;
+  })
+});
+
