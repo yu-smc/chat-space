a02d770def4c7baed4ef40107f1e8f4196ebe04e
yu <yu.smc.roundabout@gmail.com>
2018-06-25 15:51:33 +0900

    [fix] edit javascript
     ”追加””削除”を押したときの処理を実装

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -3,8 +3,19 @@ $(function(){
     var html = `<div id="chat-group-user-22" class="chat-group-user clearfix">
                   <input name='chat_group[user_ids][]' type='hidden' value='22'>
                   <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加
+                  </a>
                 </div>`
     $("#user-search-result").append(html);
+  }
+  function appendUserList(user_id, user_name) {
+
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value=${ user_id }>
+                  <p class='chat-group-user__name'>${ user_name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    $("#chat-group-users").append(html);
   }
   var ajaxSearch = function(input){
     $("#user-search-result").empty();
@@ -30,6 +41,15 @@ $(function(){
   $("#user-search-field").on('keyup', function(){
     var input = $.trim($(this).val());
     setTimeout(function(){ajaxSearch(input)}, 500);
+  });
+  $('#user-search-result').on('click', ".user-search-add", function(){
+    var user_id = $(this).attr('data-user-id'),
+        user_name = $(this).attr('data-user-name');
+    appendUserList(user_id, user_name)
+    $(this).parent().remove();
+  })
+  $('#chat-group-users').on('click', ".user-search-remove", function(){
+    $(this).parent().remove();
   });
 });
 
