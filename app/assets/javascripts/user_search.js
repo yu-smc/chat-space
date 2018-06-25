f84c8b6959c1ff638952f91ccb549c6884bedafe
yu <yu.smc.roundabout@gmail.com>
2018-06-25 21:54:01 +0900

    [fix] valueを削除

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,7 +1,7 @@
 $(function(){
   function appendUser(user){
     var html = `<div class="chat-group-user clearfix">
-                  <input name='chat_group[user_ids][]' type='hidden' value='22'>
+                  <input name='chat_group[user_ids][]' type='hidden'>
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加
                   </a>
