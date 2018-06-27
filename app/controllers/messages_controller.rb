8624fa5d10899cf3c53c24a26309974d15c2425e
yu <yu.smc.roundabout@gmail.com>
2018-06-27 11:44:23 +0900

    [add] コントローラにajax用の処理を追記

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,11 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user)
+    @messages = @group.messages.order(created_at: :ASC).includes(:user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
