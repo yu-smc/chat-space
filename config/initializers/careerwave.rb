34b969b2c43090abea3be494f94050604376e94f
yu <yu.smc.roundabout@gmail.com>
2018-07-13 19:11:31 +0900

    [fix]

--- config/initializers/careerwave.rb
+++ config/initializers/careerwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chat-space-images111'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-images111
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-images111'
 end
